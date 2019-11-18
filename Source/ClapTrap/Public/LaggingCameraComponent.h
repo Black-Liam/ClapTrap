49cc87135df80a62690a0c116789806d64dca19d
Liam Black <blac0413@algonquinlive.com>
2019-11-18 10:23:31 -0500

    Classes made for pawn and controller

---
+++ Source/ClapTrap/Public/LaggingCameraComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "LaggingCameraComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLAPTRAP_API ULaggingCameraComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+};
